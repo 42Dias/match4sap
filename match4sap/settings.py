08d37c720ea9b9296915790159b2ea29c879ef41
Marcos Lopes <marcos.lopes@42dias.com.br>
2021-03-25 11:15:12 -0300

    Update settings.py

--- match4sap/settings.py
+++ match4sap/settings.py
@@ -3,10 +3,10 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# if os.getenv('GAE_INSTANCE'):
-#     SECRET_KEY = os.environ.get('SECRET_KEY')
-# else:
-#SECRET_KEY = 'fd3h&uqymrcsyo(_5%&@!jbf182o5)k!)xty=fm056gk34^3v3'
+ if os.getenv('GAE_INSTANCE'):
+     SECRET_KEY = os.environ.get('SECRET_KEY')
+ else:
+SECRET_KEY = 'fd3h&uqymrcsyo(_5%&@!jbf182o5)k!)xty=fm056gk34^3v3'
 
 DEBUG = True
 
